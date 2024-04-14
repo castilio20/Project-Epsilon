23ad0f6f3f918634bddcd5bb5094cc7171e93c69
castilio20 <76228548+castilio20@users.noreply.github.com>
2024-04-14 23:12:38 +0300

    rename

--- src/main/java/com/vulp/druidcraft/common/block/entity/client/ModModelLayers.java
+++ src/main/java/com/vulp/druidcraft/common/block/entity/client/ModModelLayers.java
@@ -7,9 +7,9 @@ import net.minecraft.resources.ResourceLocation;
 public class ModModelLayers {
 
 
-    public static final ModelLayerLocation PINE_BOAT_LAYER = new ModelLayerLocation(
+    public static final ModelLayerLocation DARKWOOD_BOAT_LAYER = new ModelLayerLocation(
             new ResourceLocation(Druidcraft.MODID, "boat/darkwood"), "main");
-    public static final ModelLayerLocation PINE_CHEST_BOAT_LAYER = new ModelLayerLocation(
+    public static final ModelLayerLocation DARKWOOD_CHEST_BOAT_LAYER = new ModelLayerLocation(
             new ResourceLocation(Druidcraft.MODID, "chest_boat/darkwood"), "main");
 
 }
